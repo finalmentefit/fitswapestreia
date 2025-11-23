a9e0a9a5b21f4c91add1eaa81abb0cdf4b4e1c58
fitswapestreia <fitswapcleber@gmail.com>
2025-11-23 13:17:42 -0300

    Delete src directory

--- src/pages/Communities.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react"
-export default function Communities(){ return <div>Communities (placeholder)</div> }
--- src/pages/InstructorAnalytics.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react"
-export default function InstructorAnalytics(){ return <div>InstructorAnalytics (placeholder)</div> }
